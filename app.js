5a254645010f7889b0c19a4047958c3ad5de26af
Pankaj Niroula <pankajniroula365@gmail.com>
2018-10-14 22:17:13 +0545

    Added Next_Gen and Clear buttons

--- app.js
+++ app.js
@@ -24,4 +24,19 @@ var generate_table = function(r, c){
     }
     tbl.appendChild(tbl_body);
     body.appendChild(tbl);
+
+    var btn = document.createElement("button");
+    btn.innerText = "Next Gen";
+    btn.id = "next_gen_btn";
+    body.appendChild(btn);
+
+    var btn2 = document.createElement("button");
+    btn2.innerText = "Clear";
+    btn2.id = "clear_btn";
+    btn2.addEventListener("click", function(){
+        var cells = document.getElementsByTagName("td");
+        for(var i=0; i<cells.length; i++)
+            cells[i].className = "inactive";
+    });
+    body.appendChild(btn2);
 }
